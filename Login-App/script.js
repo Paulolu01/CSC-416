aee76bda0e09af279101c4b405070f45e4cd4c91
Paulolu01 <157812292+Paulolu01@users.noreply.github.com>
2026-06-11 21:15:54 +0100

    Login

---
+++ Login-App/script.js
@@ -0,0 +1,48 @@
+function login(){
+
+    let username =
+    document.getElementById("username").value;
+
+    let password =
+    document.getElementById("password").value;
+
+    let message =
+    document.getElementById("message");
+
+    if(username === "" || password === ""){
+
+        message.innerHTML =
+        "Please fill all fields";
+
+        message.style.color = "orange";
+
+        return;
+    }
+
+    if(username === "admin" &&
+       password === "1234"){
+
+        message.innerHTML =
+        "Login Successful";
+
+        message.style.color =
+        "green";
+
+    }else{
+
+        message.innerHTML =
+        "Invalid Username or Password";
+
+        message.style.color =
+        "red";
+    }
+}
+
+function resetForm(){
+
+    document.getElementById("username").value = "";
+
+    document.getElementById("password").value = "";
+
+    document.getElementById("message").innerHTML = "";
+}
